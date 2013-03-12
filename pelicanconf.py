e7c7a2ffa08fd3e8fb0c260686bfb5b8998bc640
Tomas Strand <tomas@ae-servu.net>
2013-03-12 17:07:45 +0200

    Pygments css

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,6 +14,8 @@ THEME = 'theme'
 STATIC_PATHS = ["images", ]
 MARKUP = 'md'
 
+PLUGINS = ['plugins.latex.latex',]
+
 # Should we show a vCard of the author, its only visible in the source
 VCARD = True
 AUTHOR_ADDRESS = (
